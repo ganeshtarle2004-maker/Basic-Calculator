2cc72c0a0ca55037a65fec6b01a2a37a37b3b1d7
ganeshtarle2004-maker <ganeshtarle2004@gmail.com>
2025-08-28 15:00:02 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,18 @@
+print("**welcome hallo,how can help you today?**")
+print("\nwelcome to calculator!\n")
+x=int(input("enter a first number:"))
+z=(input("enter a oparator(+,-,*,/,%"))
+y=int(input("enter a second number:"))
+if z=="+":
+    print("total add is",x+y)
+elif z=="-":
+    print("total sub is",x-y)
+elif z=="*":
+    print("total mult is",x*y)
+elif z=="/":
+    print("total diva is",x/y)
+elif z=="%":
+    print("total modul is",x%y*100)
+else:
+    print("you enter wrong operation")
+print("\n*thank you for use calculator*\n") 
